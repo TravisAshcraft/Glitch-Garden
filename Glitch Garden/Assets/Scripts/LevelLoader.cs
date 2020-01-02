e4f33ccf29da93f567e400bbea4686eefa2a6c0e
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-01-01 22:25:59 -0600

    Added Scenes and Sound
    Added Load and Start Scenes created coroutine to instantiate start screen.
    Added load sounds to both scenes.

---
+++ Glitch Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int delayInSceneLoading = 4;
+    int currentSceneIndex;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(SceneLoader());
+        }
+        
+
+    }
+
+    IEnumerator SceneLoader()
+    {
+        yield return new WaitForSeconds(delayInSceneLoading);
+        LoadNextScene();
+        
+        
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
