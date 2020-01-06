03f585b7836df2360a06ae8da9105ae46c354253
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-01-05 21:03:28 -0600

    added better spawning for defenders

--- Glitch Garden/Assets/Scripts/DefenderSpawner.cs
+++ Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -8,11 +8,27 @@ public class DefenderSpawner : MonoBehaviour
 
     public void OnMouseDown()
     {
-        SpawnDefender();
+        SpawnDefender(GetSquareClicked());
     }
 
-    private void SpawnDefender()
+    private Vector2 GetSquareClicked()
     {
-        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity);
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+    private void SpawnDefender(Vector2 roundedPos)
+    {
+      
+        GameObject newDefender = Instantiate(defender, roundedPos, Quaternion.identity);
+        Debug.Log(roundedPos);
     }
 }
