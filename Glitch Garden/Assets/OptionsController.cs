6410de1f11ca102b5be4bc711dc2aa89873e8705
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-01-12 21:15:51 -0600

    Added volume with slider controller

---
+++ Glitch Garden/Assets/OptionsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaltVolume = 0.6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("no music player found, start from splash screen");
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+
+        
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaltVolume;
+    }
+
+}
